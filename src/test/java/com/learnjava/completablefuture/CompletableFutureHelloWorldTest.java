b342fcdf4563b0e29fd0de96fc17d873a5b4b654
Xiaojun Yang <csgear@live.com>
2022-07-04 08:21:31 -0400

    More examples

--- src/test/java/com/learnjava/completablefuture/CompletableFutureHelloWorldTest.java
+++ src/test/java/com/learnjava/completablefuture/CompletableFutureHelloWorldTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,4 +27,74 @@ class CompletableFutureHelloWorldTest {
                 .join();
     }
 
+    @Test
+    void supplyAsync() throws ExecutionException, InterruptedException {
+        CompletableFuture<String>  future = CompletableFuture.supplyAsync(
+                () -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1) ;
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return "Result of the asynchronous computation";
+                }
+        ) ;
+
+        String result = future.get() ;
+        assertEquals(result, "Result of the asynchronous computation");
+    }
+
+    @Test
+    void supplyAsyncWithThreadPool() throws ExecutionException, InterruptedException {
+        Executor executor = Executors.newFixedThreadPool(10);
+        CompletableFuture<String>  future = CompletableFuture.supplyAsync(
+                () -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1) ;
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return "Result of the asynchronous computation";
+                }, executor
+        ) ;
+
+        String result = future.get() ;
+        assertEquals(result, "Result of the asynchronous computation");
+    }
+
+    @Test
+    void anyOf() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Result of Future 1";
+        });
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Result of Future 2";
+        });
+
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Result of Future 3";
+        });
+
+        CompletableFuture<Object> anyOfFuture = CompletableFuture.anyOf(future1, future2, future3) ;
+
+        String expected = "Result of Future 2" ;
+
+        assertEquals(expected, anyOfFuture.get());
+    }
 }
